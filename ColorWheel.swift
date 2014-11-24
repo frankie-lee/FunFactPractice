7fb814e78fff4d51411140042148ae9d3dcd6433
Frank Lee <flee@merscient.com>
2014-11-24 16:28:29 -0500

    first commit

---
+++ ColorWheel.swift
@@ -0,0 +1,9 @@
+//
+//  ColorWheel.swift
+//  FunFacts
+//
+//  Created by Frank Lee on 2014-10-17.
+//  Copyright (c) 2014 franklee. All rights reserved.
+//
+
+import Foundation
---
+++ FunFacts/FactBook.swift
@@ -0,0 +1,9 @@
+//
+//  FactBook.swift
+//  FunFacts
+//
+//  Created by Frank Lee on 2014-10-17.
+//  Copyright (c) 2014 franklee. All rights reserved.
+//
+
+import Foundation
